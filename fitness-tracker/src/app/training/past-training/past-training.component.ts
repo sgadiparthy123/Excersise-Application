28f0edfd17031bd88245722a1dce69667ad84110
Sri kanth Gadiparthy <sreekanthgadiparthy@gmail.com>
2020-12-21 22:47:23 -0500

    Data table changes

--- fitness-tracker/src/app/training/past-training/past-training.component.ts
+++ fitness-tracker/src/app/training/past-training/past-training.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { Exercise } from '../exercise.model';
+import { TrainingService } from '../training.service';
 
 @Component({
   selector: 'app-past-training',
   templateUrl: './past-training.component.html',
   styleUrls: ['./past-training.component.scss']
 })
-export class PastTrainingComponent implements OnInit {
-
-  constructor() { }
+export class PastTrainingComponent implements OnInit, AfterViewInit {
+  displayedColumns = ['date', 'name', 'duration', 'calories', 'state'];
+  dataSource = new MatTableDataSource<Exercise>();
+  @ViewChild(MatSort) sort : MatSort;
+  @ViewChild(MatPaginator) paginator : MatPaginator;
+  
+  constructor(private trainingService: TrainingService) { }
+  
+  ngAfterViewInit(): void {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+  }
 
   ngOnInit(): void {
+    this.dataSource.data = this.trainingService.getCompletedOrCanceledExercises();
+  }
+
+  doFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
 }
